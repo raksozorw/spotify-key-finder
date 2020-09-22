a98d3cd0ae2e8fb23ab1bf59f972473c62883390
Oskar Wroz <oskarwroz@gmail.com>
2020-09-22 09:20:58 +0100

    first commit, functions locally

---
+++ authorization_code/build/precache-manifest.1ffcefc24d20f6add0cc81dd8d0db320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f213c21f13258e5f90eb6ba0033293d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82ab1fc110afdbaafd78",
+    "url": "/static/css/main.8d113259.chunk.css"
+  },
+  {
+    "revision": "99d016c52ff401e700ea",
+    "url": "/static/js/2.3d836b26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d836b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82ab1fc110afdbaafd78",
+    "url": "/static/js/main.182073c6.chunk.js"
+  },
+  {
+    "revision": "9af09f7ba5870b879a37",
+    "url": "/static/js/runtime-main.63fe1708.js"
+  },
+  {
+    "revision": "49297da4a81cbde79accd99b45dc06c1",
+    "url": "/static/media/42672108_2031052090251354_8552365379190521856_o.49297da4.jpg"
+  }
+]);
